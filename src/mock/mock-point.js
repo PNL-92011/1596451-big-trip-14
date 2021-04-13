087a139d4ff1acf7124cf7aa4be2ce546805fea9
Mary <maria79264163641@gmail.com>
2021-04-13 18:43:10 +0300

    Mock-2 (offers)

--- src/mock/mock-point.js
+++ src/mock/mock-point.js
@@ -7,6 +7,7 @@ const PHOTO_LENGTH_MAX = 100;
 const PHOTO_AMOUNT_MIN = 1;
 const PHOTO_AMOUNT_MAX = 5;
 const PHOTO_URL = 'http://picsum.photos/248/152?r=';
+const MAX_OFFERS = 5;
 
 const TYPES = ['Taxi', 'Bus', 'Train', 'Ship', 'Transport', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
 const CITIES = ['Chamonix', 'Rotterdam', 'Toulouse', 'Paris', 'Zurich', 'Madrid', 'Budapest', 'Izmir', 'Milan'];
@@ -23,6 +24,29 @@ const DESCRIPTIONS = [
   'Aliquam erat volutpat.',
   'Nunc fermentum tortor ac porta dapibus.',
   'In rutrum ac purus sit amet tempus.',
+];
+
+const OFFERS = [
+  {
+    name: 'Add luggage',
+    price: 30,
+  },
+  {
+    name: 'Switch to comfort class',
+    price: 100,
+  },
+  {
+    name: 'Add meal',
+    price: 15,
+  },
+  {
+    name: 'Choose seats',
+    price: 5,
+  },
+  {
+    name: 'Travel by train',
+    price: 40,
+  },
 ];
 
 /**
@@ -62,26 +86,39 @@ const generateDate = () => {
 // нужно ли использовать библиотеку flatpickr.js?  где?
 
 
-const generatePoint = () => {
+const generateNewArr = ([...source], maxLength) => Array.from(
+  { length: Math.min(source.length, 1 + Math.random() * maxLength | 0) },
+  () => source.splice(Math.random() * source.length | 0, 1)[0],
+);
+
+
+const generateOffers = () => {
+  const isOffers = Boolean(getRandomInteger(0, 1));
+  if(!isOffers) {
+    return null;
+  } else {
+    return generateNewArr(OFFERS, MAX_OFFERS);
+  }
+};
+
+export const generatePoint = () => {
   return {
     type: getRandomElement(TYPES),
     trip_info: {
       name: getRandomElement(CITIES),
-      // После выбора пункта назначения появляется блок «Destination». В нём отображается информация о месте назначения (изображения, текст).
-      // Информация в блоке «Destination» всегда соответствует выбранному пункту назначения.
+
       destination: {
         description: getShuffled(DESCRIPTIONS).slice(0, 5),
         photos: generatePhotos(),
       },
     },
-    // Дата и время начала события. Выбор времени и даты осуществляется с помощью библиотеки flatpickr.js.
-    // Выбранная дата и время отображаются в поле в формате: день/месяц/год часы:минуты (например «25/12/2019 16:00»).
-    // Дата и время окончания события. Формат и требования аналогичны дате начала. Дата окончания не может быть меньше даты начала события.
+
     date_from: generateDate(),
     date_till: generateDate(),
     cost: getRandomInteger(0, 100),
 
-    // offers: offers, boolean
+    //offers: offers, boolean
+    offers: generateOffers(),
 
 
     isFavorite: Boolean(getRandomInteger(0, 1)),
@@ -91,4 +128,12 @@ const generatePoint = () => {
 
 console.log(generatePoint());
 
-export {generatePoint};
+
+// Пометки:
+
+// После выбора пункта назначения появляется блок «Destination». В нём отображается информация о месте назначения (изображения, текст).
+// Информация в блоке «Destination» всегда соответствует выбранному пункту назначения.
+
+// Дата и время начала события. Выбор времени и даты осуществляется с помощью библиотеки flatpickr.js.
+// Выбранная дата и время отображаются в поле в формате: день/месяц/год часы:минуты (например «25/12/2019 16:00»).
+// Дата и время окончания события. Формат и требования аналогичны дате начала. Дата окончания не может быть меньше даты начала события.
